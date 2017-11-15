cedd4294fe2d92ffa2d5f4401a8f6f6ff30ae968
Guillaume Coré <gucore@redhat.com>
2017-11-15 11:56:19 +0100

    fix_ipa: ensure it keeps working for a while

--- ansible/roles/bastion-opentlc-ipa/files/fix_ipa.sh
+++ ansible/roles/bastion-opentlc-ipa/files/fix_ipa.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-systemctl restart sssd
-sleep 10
 
-for i in $(seq 20); do
+MAX=20
+
+# ensure it keeps working for a while
+for i in $(seq $MAX); do
     /usr/bin/sss_ssh_authorizedkeys jenkins-sfo01
     RET=$?
-    if [ $RET = 0 ]; then exit 0; fi
-
-    systemctl restart sssd
+    if [ $RET != 0 ]; then
+        systemctl restart sssd
+    fi
     sleep 10
 done
-
-exit 2
